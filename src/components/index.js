67c108ec476d68aecf587e47111b677c800a4589
Mason <masonsepulveda@gmail.com>
2022-12-12 18:35:53 -0600

    Add existing project files to Git

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Stats from "./Stats";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import CTA from "./CTA";
+import Clients from "./Clients";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+
+export {
+  Navbar,
+  Billing,
+  Stats,
+  Hero,
+  Footer,
+  Testimonials,
+  CTA,
+  Clients,
+  Business,
+  Button,
+  CardDeal,
+};
